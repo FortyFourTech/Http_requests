d1dcf6b2d4c9d4130660f888038cf228c46a76f3
Dimar <fortyfourtech@gmail.com>
2018-02-15 12:13:05 +0700

    replaced requests

--- Assets/Code/Networking/Requests.cs
+++ Assets/Code/Networking/Requests.cs
@@ -1,50 +1,105 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 using UnityEngine.Networking;
-using UnityEngine.Assertions;
+// using UnityEngine.Assertions;
 
 namespace API {
     public enum Request {
-        authorize,
-        playSound,
-        getCurrent,
+        /// <summary>
+        /// https://translate.yandex.net/api/v1.5/tr.json/getLangs
+        /// ? [key=<API-ключ>]
+        /// & [ui=<код языка>]
+        /// & [callback=<имя callback-функции>]
+        /// </summary>
+        languageList,
+        /// <summary>
+        /// https://translate.yandex.net/api/v1.5/tr.json/detect
+        /// ? [key=<API-ключ>]
+        /// & text=<текст>
+        /// & [hint=<список вероятных языков текста>]
+        /// & [callback=<имя callback-функции>]
+        /// </summary>
+        detectLanguage,
+        /// <summary>
+        /// https://translate.yandex.net/api/v1.5/tr.json/translate
+        ///  ? [key=<API-ключ>]
+        ///  & [text=<переводимый текст>]
+        ///  & [lang=<направление перевода>]
+        ///  & [format=<формат текста>]
+        ///  & [options=<опции перевода>]
+        ///  & [callback=<имя callback-функции>]
+        /// </summary>
+        translate,
     }
 
     static public class RequestExtension {
         public static string url(this Request rq) {
             string result = NetworkService.BASE_URL;
             switch (rq) {
-                case Request.authorize:
-                    result += "/auth_login";
+                case Request.languageList:
+                    result += "/getLangs";
                     break;
-                case Request.playSound:
-                    result += "/quest_control";
+                case Request.detectLanguage:
+                    result += "/detect";
                     break;
-                case Request.getCurrent:
-                    result += "";
+                case Request.translate:
+                    result += "/translate";
                     break;
                 default:
-                    Assert.IsTrue(false, "url for request '" + rq.ToString() + "' is not defined");
-                    break;
+                    throw new Exception("url for request '" + rq.ToString() + "' is not defined");
             }
 
             return result;
         }
 
-        public static string method(this Request rq) {
-            string result = "";
+        public static RequestParameter[] getParameters(this Request rq) {
+            RequestParameter[] result;
             switch (rq) {
-                case Request.authorize:
-                case Request.playSound:
-                    result = UnityWebRequest.kHttpVerbPOST;
+                case Request.languageList:
+                    result = new RequestParameter[] {
+                        RequestParameter.key,
+                        RequestParameter.ui,
+                    };
                     break;
-                case Request.getCurrent:
-                    result = UnityWebRequest.kHttpVerbGET;
+                case Request.detectLanguage:
+                    result = new RequestParameter[] {
+                        RequestParameter.key,
+                        RequestParameter.hint,
+                    };
+                    break;
+                case Request.translate:
+                    result = new RequestParameter[] {
+                        RequestParameter.key,
+                        RequestParameter.lang,
+                    };
                     break;
                 default:
-                    Assert.IsTrue(false, "method for request '" + rq.ToString() + "' is not defined");
+                    throw new Exception("method for request '" + rq.ToString() + "' is not defined");
+            }
+
+            return result;
+        }
+
+        public static RequestParameter[] postParameters(this Request rq) {
+            RequestParameter[] result;
+            switch (rq) {
+                case Request.languageList:
+                    result = new RequestParameter[] { };
                     break;
+                case Request.detectLanguage:
+                    result = new RequestParameter[] {
+                        RequestParameter.text,
+                    };
+                    break;
+                case Request.translate:
+                    result = new RequestParameter[] {
+                        RequestParameter.text,
+                    };
+                    break;
+                default:
+                    throw new Exception("method for request '" + rq.ToString() + "' is not defined");
             }
 
             return result;
